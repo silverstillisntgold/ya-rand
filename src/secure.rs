c1acdc467f14efa0b98e4dbb3b555fed1cbc1044
silverstillisntgold <nickdilla13@gmail.com>
2026-08-10 22:59:31 -0400

    use new `chachacha` version
    Now uses 10-round ChaCha instead of 8. Also used newly available `cold_path` to tell the compiler our if-branch in `u64` is infrequently called. Should repeated calls to the generator faster.

--- src/secure.rs
+++ src/secure.rs
@@ -1,17 +1,30 @@
 use crate::rng::{Generator, SecureGenerator};
-use chachacha::{BUF_LEN_U64, ChaCha8Djb, SEED_LEN_U8};
+use chachacha::{BATCH_BYTES, ChaChaDjb};
 use core::fmt;
-use core::mem::MaybeUninit;
+
+type ChaCha = ChaChaDjb<10>;
+
+union Buffer {
+    u8: [u8; BATCH_BYTES],
+    u64: [u64; BATCH_BYTES / size_of::<u64>()],
+}
+
+impl Default for Buffer {
+    #[inline]
+    fn default() -> Self {
+        Self { u8: [0; _] }
+    }
+}
 
 /// A cryptographically secure random number generator.
 ///
-/// The current implementation uses ChaCha with 8 rounds and a 64-bit counter.
+/// The current implementation uses ChaCha with 10 rounds and a 64-bit counter.
 /// This allows for 1 ZiB (2<sup>70</sup> bytes) of output before repeating.
 /// That's over 147 **quintillion** calls to [`SecureRng::u64`].
 pub struct SecureRng {
-    buf: [u64; BUF_LEN_U64],
+    buffer: Buffer,
+    rng: ChaCha,
     index: usize,
-    internal: ChaCha8Djb,
 }
 
 impl fmt::Debug for SecureRng {
@@ -25,39 +38,41 @@ impl SecureGenerator for SecureRng {
     fn fill_bytes(&mut self, dst: &mut [u8]) {
         // The `chachacha` crate provides a thoroughly tested and
         // extremely fast fill implementation.
-        self.internal.fill(dst);
+        self.rng.fill(dst);
     }
 }
 
 impl Generator for SecureRng {
-    #[inline]
+    #[inline(never)]
     fn try_new() -> Result<Self, getrandom::Error> {
         // We want to randomize **all** bits of the matrix, even the counter.
-        // They're all going to be overwritten so no need to initialize them.
-        #[allow(clippy::uninit_assumed_init, invalid_value)]
-        let mut state = unsafe { MaybeUninit::<[u8; SEED_LEN_U8]>::uninit().assume_init() };
+        let mut state = [0; _];
         getrandom::fill(&mut state)?;
-        let mut internal = ChaCha8Djb::from(state);
-        let buf = internal.get_block_u64();
+        let mut buffer = Buffer::default();
+        let mut rng = ChaCha::from_bytes(state);
+        unsafe {
+            rng.fill_exact(&mut buffer.u8);
+        }
         let index = 0;
-        Ok(Self {
-            buf,
-            index,
-            internal,
-        })
+        Ok(Self { buffer, rng, index })
     }
 
     #[cfg_attr(feature = "inline", inline)]
     fn u64(&mut self) -> u64 {
-        // TODO: Use the `unlikely` hint when it comes to stable.
-        if self.index >= self.buf.len() {
-            self.index = 0;
-            self.internal.fill_block_u64(&mut self.buf);
+        unsafe {
+            if self.index >= self.buffer.u64.len() {
+                // This branch only occurs when `self.buffer.u64` has
+                // been exhausted: once every 32 calls.
+                core::hint::cold_path();
+                self.rng.fill_exact(&mut self.buffer.u8);
+                // Needs to be zeroed **after** `self.buffer.u8` is filled.
+                self.index = 0;
+            }
+            // Control flow of the above if-statement ensures
+            // this bounds check is optimized out.
+            let ret = self.buffer.u64[self.index];
+            self.index += 1;
+            ret
         }
-        // SAFETY: We've just guaranteed that `self.index` will be
-        // in bounds in the above if-statement.
-        let ret = unsafe { *self.buf.get_unchecked(self.index) };
-        self.index += 1;
-        ret
     }
 }
